37d1f54ed621eec0ba4f745b2bb49ca164951243
rnyoung1230 <robert_neil_young@outlook.com>
2025-05-01 13:36:50 -0500

    Working with data and data models

--- hello/views.py
+++ hello/views.py
@@ -2,19 +2,37 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
-#def home(request):
-#    return HttpResponse("Hello, Django!")
 
-# Replace the existing home function with the one below
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def about(request):
     return render(request, "hello/about.html")
 
 def contact(request):
     return render(request, "hello/contact.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
 
 '''def hello_there(request, name):
     now = datetime.now()
